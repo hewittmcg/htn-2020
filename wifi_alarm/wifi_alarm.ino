a50ae3f9c23e8e4ac797a2b7418e0e9c4e453a25
Hewitt McGaughey <hewittmcgaughey@gmail.com>
2021-01-16 12:04:55 -0500

    rough demo code mostly works

--- wifi_alarm/wifi_alarm.ino
+++ wifi_alarm/wifi_alarm.ino
@@ -8,8 +8,8 @@
 
 // WiFi info
 // DELETE BEFORE COMMITTING TO SOURCE CONTROL!!!
-const char *ssid = "McGaughey";
-const char *pass = "!Algonquin99";
+const char *ssid = "";
+const char *pass = "";
 
 // IP of MQTT server
 const char *server_ip = "192.168.2.89";
@@ -18,8 +18,19 @@ const int server_port = 1883;
 // MQTT topics used for tx/rx
 // (todo)
 
+// todo replace these with bytecodes later to make simpler
+// message sent to turn on buzzer
+const char *ALARM_ON_MSG = "alarmon";
+
+// message sent to turn off alarm on app
+const char *ALARM_OFF_MSG = "alarmoff";
+
 WiFiClient wifi_client;
 PubSubClient mqtt_client(wifi_client);
+
+// IO
+const int LED_PIN = 13;
+const int BUTTON_PIN = 14;
 
 void setup() {
   Serial.begin(115200);
@@ -45,17 +56,40 @@ void setup() {
   Serial.println("Connected to MQTT.");
 
   mqtt_client.subscribe("test_rx/");
+
+  // Configure IO
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
 }
 
 // Called when a MQTT message is received
+// used to turn on alarm
 void callback(char *topic, byte *payload, unsigned int length) {
   Serial.println("received message:");
-  Serial.println((char*)payload);
+
+  // print this way to deal with weird overflow errors
+  for(int i = 0; i < length; i++) {
+    Serial.print(char(payload[i]));
+  }
+  Serial.println("");
+
+  Serial.println(length);
+  if(memcmp((char*)payload, ALARM_ON_MSG, length) == 0) {
+    Serial.println("Alarm on, waiting for button press");
+    //todo
+    // turn on LED/buzzer
+    digitalWrite(LED_PIN, HIGH);
+
+    // wait in busy loop for button to be pressed
+    while(digitalRead(BUTTON_PIN) == LOW) {
+      delay(50);
+    }
+    digitalWrite(LED_PIN, LOW);
+    Serial.println("Button press registered, alarm turned off");
+  }
+  
 }
 
 void loop() {
-  char *test_msg = "testing whooooooo";
-  Serial.println("Sending test msg");
-  mqtt_client.publish("test_tx/", test_msg);
-  delay(5000);
+  mqtt_client.loop();
 }
